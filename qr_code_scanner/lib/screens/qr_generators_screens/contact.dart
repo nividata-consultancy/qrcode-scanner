74ac4dee06da7fb72b4357d7da9b0dcbf70a7956
Mehul Makwana <mehulmk@nividata.com>
2020-03-26 13:45:31 +0530

    add qrShareDialog utility

--- qr_code_scanner/lib/screens/qr_generators_screens/contact.dart
+++ qr_code_scanner/lib/screens/qr_generators_screens/contact.dart
@@ -1,15 +1,11 @@
-import 'dart:developer';
-import 'dart:io';
-import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:qr_code_scanner/res/strings.dart';
-import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_code_scanner/screens/qr_generators_screens/qrShareDialog.dart';
 
 class ContactQR extends StatefulWidget {
   @override
@@ -60,9 +56,6 @@ class _PhoneNumberScreenState extends State<PhoneNumberScreen> {
 
   String _dataString = "Hello from this QR";
   String _inputErrorText;
-  var bodyHeight;
-
-  GlobalKey globalKey = new GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -225,76 +218,11 @@ class _PhoneNumberScreenState extends State<PhoneNumberScreen> {
     );
   }
 
-  Future<void> _captureAndSharePng() async {
-    try {
-      print("ok");
-      RenderRepaintBoundary boundary =
-          globalKey.currentContext.findRenderObject();
-      var image = await boundary.toImage();
-      ByteData byteData = await image.toByteData(format: ImageByteFormat.png);
-      Uint8List pngBytes = byteData.buffer.asUint8List();
-
-      final tempDir = await getTemporaryDirectory();
-      final file = await new File('${tempDir.path}/image.png').create();
-      await file.writeAsBytes(pngBytes);
-
-      final channel = const MethodChannel('channel:me.alfian.share/share');
-      channel.invokeMethod('shareFile', 'image.png');
-    } catch (e) {
-      print(e.toString());
-      log(e);
-    }
-  }
-
   void phoneInputBS(_dataString, context) {
+    var bodyHeight = MediaQuery.of(context).size.height -
+        MediaQuery.of(context).viewInsets.bottom;
     GlobalKey globalKey = new GlobalKey();
 
-    bodyHeight = MediaQuery.of(context).size.height -
-        MediaQuery.of(context).viewInsets.bottom;
-    showModalBottomSheet(
-        backgroundColor: Colors.transparent,
-        context: context,
-        builder: (BuildContext bc) {
-          return Container(
-              height: 450.0,
-              margin: EdgeInsets.all(10),
-              color:
-                  Colors.transparent, //could change this to Color(0xFF737373),
-              //so you don't have to change MaterialApp canvasColor
-              child: new Container(
-                decoration: new BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: new BorderRadius.all(Radius.circular(10.0))),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: RepaintBoundary(
-                        key: globalKey,
-                        child: QrImage(
-                          version: QrVersions.auto,
-                          gapless: false,
-                          data: _dataString,
-                          size: 0.38 * bodyHeight,
-                        ),
-                      ),
-                    ),
-                    MaterialButton(
-                      color: Colors.deepOrange,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6.0)),
-                      child: Text(
-                        "Share",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () {
-                        log("ok123");
-                        _captureAndSharePng();
-                      },
-                    )
-                  ],
-                ),
-              ));
-        });
+    QrShareDialog.showDialog(context, bodyHeight, _dataString, globalKey);
   }
 }
--- qr_code_scanner/lib/screens/qr_generators_screens/email.dart
+++ qr_code_scanner/lib/screens/qr_generators_screens/email.dart
@@ -1,14 +1,11 @@
-import 'dart:io';
-import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:qr_code_scanner/res/strings.dart';
-import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_code_scanner/screens/qr_generators_screens/qrShareDialog.dart';
 
 class EmailQR extends StatefulWidget {
   @override
@@ -55,8 +52,6 @@ class _EmailScreenState extends State<EmailScreen> {
 
   String _dataString = "Hello from this QR";
   String _inputErrorText;
-  var bodyHeight;
-  GlobalKey globalKey = new GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -165,71 +160,11 @@ class _EmailScreenState extends State<EmailScreen> {
     );
   }
 
-  Future<void> _captureAndSharePng() async {
-    try {
-      RenderRepaintBoundary boundary =
-          globalKey.currentContext.findRenderObject();
-      var image = await boundary.toImage();
-      ByteData byteData = await image.toByteData(format: ImageByteFormat.png);
-      Uint8List pngBytes = byteData.buffer.asUint8List();
-
-      final tempDir = await getTemporaryDirectory();
-      final file = await new File('${tempDir.path}/image.png').create();
-      await file.writeAsBytes(pngBytes);
-
-      final channel = const MethodChannel('channel:me.alfian.share/share');
-      channel.invokeMethod('shareFile', 'image.png');
-    } catch (e) {
-      print(e.toString());
-    }
-  }
-
   void phoneInputBS(_dataString, context) {
-    bodyHeight = MediaQuery.of(context).size.height -
+    var bodyHeight = MediaQuery.of(context).size.height -
         MediaQuery.of(context).viewInsets.bottom;
-    showModalBottomSheet(
-        backgroundColor: Colors.transparent,
-        context: context,
-        builder: (BuildContext bc) {
-          return Container(
-              height: MediaQuery.of(context).size.height,
-              margin: EdgeInsets.all(10.0),
-              color:
-                  Colors.transparent, //could change this to Color(0xFF737373),
-              //so you don't have to change MaterialApp canvasColor
-              child: new Container(
-                decoration: new BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: new BorderRadius.all(Radius.circular(10.0))),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: RepaintBoundary(
-                        key: globalKey,
-                        child: QrImage(
-                          gapless: false,
-                          version: QrVersions.auto,
-                          data: _dataString,
-                          size: 0.38 * bodyHeight,
-                        ),
-                      ),
-                    ),
-                    MaterialButton(
-                      color: Colors.deepOrange,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6.0)),
-                      child: Text(
-                        "Share",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () {
-                        _captureAndSharePng();
-                      },
-                    )
-                  ],
-                ),
-              ));
-        });
+    GlobalKey globalKey = new GlobalKey();
+
+    QrShareDialog.showDialog(context, bodyHeight, _dataString, globalKey);
   }
 }
